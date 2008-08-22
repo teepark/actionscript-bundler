04277078dcfb9d175b9e245c29f616cddeeaf5c5
Travis J Parker <travis.parker@gmail.com>
2008-08-22 15:46:34 -0700

    lots of bugfixes, utility works now

--- actionscript3bundler.py
+++ actionscript3bundler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
-import sys
 import os
 import re
+import sys
+import zipfile
 
 
 IGNORED_TOPLEVELS = ("adobe", "flash", "fl", "mx")
@@ -10,38 +11,53 @@ IGNORED_TOPLEVELS = ("adobe", "flash", "fl", "mx")
 import_re = re.compile(r"import(?: )+((?:\w+\.)*)(\w+);")
 starimport_re = re.compile(r"import(?: )+((?:\w+\.)*)\*;")
 
-package_re = re.compile(r"package(?: )+ ((?\w+\.)*\w+)")
-as2class_re = re.compile(r"class(?: )+ ((?\w+\.)*\w+)")
+package_re = re.compile(r"package(?: )+((?:\w+\.)*\w+)")
+#as2class_re = re.compile(r"class(?: )+((?:\w+\.)*\w+)")
 
-files = {}
+files = []
 folders = []
 top = ""
+zfile = None
 
 def process_all():
+    globals()["zfile"] = zipfile.ZipFile("ASBundle.zip", "w")
     for start in (sys.argv[1:] or (".",)):
         if os.path.isfile(start) and start.endswith(".as"):
             process_actionscript(start)
         elif os.path.isdir(start):
             process_folder(start)
+    zfile.close()
 
 def process_actionscript(location):
     if location in files:
         return
 
     f = open(location, "r")
-    text = f.read()
+    rawtext = f.read()
     f.close()
 
-    files[location] = text
+    files.append(location)
 
-    text = strip_multiline_comments(text)
+    text = strip_multiline_comments(rawtext)
     text = strip_singleline_comments(text)
+
+    for line in text.splitlines():
+        match = package_re.search(line)
+        if match:
+            package = "".join(match.groups())
+            break
+    else:
+        raise Exception("no package statement in AS file '" + location + "'")
+    compath = "%s/%s" % (os.sep.join(package.split(".")),
+                            os.path.basename(location))
+
+    zfile.writestr(compath, rawtext)
 
     if not top:
         find_top(location, text)
 
     imports, starimports = [], []
-    for line in text.split():
+    for line in text.splitlines():
         match = import_re.search(line)
         if match:
             imports.append("".join(match.groups()))
@@ -64,11 +80,11 @@ def process_folder(location):
     folders.append(location)
 
     for i in [f for f in os.listdir(location) if f.endswith("as")]:
-        process_actionscript(i)
+        process_actionscript(os.path.join(location, i))
 
 def strip_singleline_comments(text):
     results = []
-    for line in text.split():
+    for line in text.splitlines():
         start = line.find("//")
         if start + 1: results.append(line[:start])
         else: results.append(line)
@@ -81,11 +97,11 @@ def strip_multiline_comments(text):
         if start == -1: break
         end = ((text.find("*/", start) + 1) or (len(text) - 1)) + 1
 
-        text = text[start:] + text[:end]
+        text = text[:start] + text[end:]
     return text
 
 def find_top(asfileloc, asfiletext):
-    for line in asfiletext.split():
+    for line in asfiletext.splitlines():
         match = package_re.search(line)
         if match:
             package = "".join(match.groups())
@@ -96,7 +112,12 @@ def find_top(asfileloc, asfiletext):
 
 def get_path(dotpath):
     slashpath = dotpath.replace(".", os.sep)
-    return os.sep.join(top, slashpath)
+    return os.sep.join((top, slashpath))
+
+def build_zipfile():
+    zfile = zipfile.ZipFile("ASBundle.zip", "w")
+    for name, contents in files:
+        zfile.writestr(name, contents)
 
 
 if __name__ == "__main__":
