be959cde510d6924f595165c9b1fabeb0e26e741
Travis J Parker <travis.parker@gmail.com>
2008-08-22 11:23:49 -0700

    finished recursive folder and AS-file parsing

--- actionscript3bundler.py
+++ actionscript3bundler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
+import sys
 import os
 import re
 
@@ -9,30 +9,47 @@ IGNORED_TOPLEVELS = ("adobe", "flash", "fl", "mx")
 
 import_re = re.compile(r"import(?: )+((?:\w+\.)*)(\w+);")
 starimport_re = re.compile(r"import(?: )+((?:\w+\.)*)\*;")
-linecomment_re = re.compile(r"//")
 
 files = {}
+
+def process_all():
+    starts = sys.argv[1:]
+    if not starts:
+        starts = (".",)
+
+    for start in starts:
+        if os.path.isfile(start) and start.endswith(".as"):
+            process_actionscript(start)
+        elif os.path.isdir(start):
+            process_folder(start)
 
 def process_actionscript(location):
     if location in files:
         return
 
     f = open(location, "r")
-    t = f.read()
+    text = f.read()
     f.close()
 
-    files[location] = t
+    files[location] = text
+
+    while 1:
+        start = text.find("/*")
+        if start == -1: break
+        end = ((text.find("*/", start) + 1) or (len(text) - 1)) + 1
+        text = text[start:] + text[:end]
 
     imports, starimports = [], []
-    for line in t.split():
-        comment = linecomment_re.search(line)
+    for line in text.split():
+        comment_start = line.find("//")
+        comment = comment_start != -1
 
         match = import_re.search(line)
-        if match and ((not comment) or comment.start() > match.end()):
+        if match and (not comment or comment_start > match.end()):
             imports.append("".join(match.groups()))
 
         match = starimport_re.search(line)
-        if match and ((not comment) or comment.start() > match.end()):
+        if match and (not comment or comment_start > match.end()):
             starimports.append(match.groups()[0][:-1])
 
     for i in imports:
@@ -48,14 +65,9 @@ def process_actionscript(location):
         process_folder(i)
 
 def process_folder(location):
-    for i in os.listdir(location):
-        i = os.path.join(location, i)
-        if i.endswith(".as") and i not in files:
-            process_actionscript(i)
+    for i in [f for f in os.listdir(location) if f.endswith("as")]:
+        process_actionscript(i)
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("-s", "--start")
-
-    options, args = parser.parse_args()
+    process_all()
