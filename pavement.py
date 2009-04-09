29badf95200d2aa0fd8890a014bda699addacd34
Travis Parker <travis@slide.com>
2009-04-08 22:56:23 -0700

    added a clean task and a little reorg

--- pavement.py
+++ pavement.py
@@ -1,13 +1,27 @@
 from paver.easy import *
-import paver.doctools
+from paver.path import path
 from paver.setuputils import setup
 
 
 setup(
     name="actionscript-bundler",
-    packages=['as3bundler'],
-    scripts=['as3bundler/as3bundler.py'],
+    scripts=['as3bundler.py'],
     version="0.1",
     author="Travis Parker",
     author_email="travis.parker@gmail.com"
 )
+
+@task
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def sdist():
+    pass
+
+@task
+def clean():
+    for p in map(path, ('actionscript_bundler.egg-info', 'dist', 'setup.py',
+                        'paver-minilib.zip', 'build')):
+        if p.exists():
+            if p.isdir():
+                p.rmtree()
+            else:
+                p.remove()
